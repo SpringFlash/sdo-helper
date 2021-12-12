f9bd760deff61633cefe243ca1c9d2eeb46d6b28
SpringFlash <vadik2001.8928@gmail.com>
2021-12-12 22:15:37 +0300

    feat: new types, google find, only correct answers + refactor

--- answer.js
+++ answer.js
@@ -1,55 +1,155 @@
-function pasteAnsw(val) {
-  const answers = JSON.parse(val);
-  const answ_nodes = [
-    ...document.querySelector("#responseform").children[0].children,
-  ].filter((el) => el.classList.contains("que"));
+/* ----------------------------------------- src/getters ------------------------------------------ */
+function getQuestionsNodes(type) {
+  const selectors = {
+    get: '.questionflagsaveform',
+    paste: '#responseform',
+  };
+  return [...document.querySelector(selectors[type]).children[0].children].filter((el) =>
+    el.classList.contains('que')
+  );
+}
 
-  for (const ans of answ_nodes) {
-    const answer = ans.querySelector(".answer");
-    const name = ans.querySelector(".qtext").innerText;
-    const answObj = answers[name];
-    if (!answObj) continue;
-    if (
-      ans.classList.contains("multichoice") ||
-      ans.classList.contains("truefalse")
-    ) {
-      for (let el of answer.children) {
-        let inp = el.querySelector(`input:not([type="hidden"])`);
-        const labelNode = el.querySelector("label");
-        const label = labelNode.cloneNode(true);
-        label.children[0]?.remove();
-        const lab = label.innerText.replace(/\u00A0|^\s+|\s+$/g, "");
-        if (answObj.includes(lab)) {
-          inp.checked = true;
-        }
-      }
-    } else if (ans.classList.contains("match")) {
-      const tbody = answer.children[0];
+function getQuestionType(question) {
+  const variants = {
+    multichoice: 'choose',
+    truefalse: 'choose',
+    match: 'match',
+    shortanswer: 'shortanswer',
+    numerical: 'shortanswer',
+  };
 
-      for (const tr of tbody.children) {
-        const p = tr.querySelector(".text p").innerText;
-        const choice = tr.querySelector("select");
-        [...choice.children].forEach((el, ind) => {
-          if (el.innerText == answObj[p]) choice.selectedIndex = ind;
-        });
-      }
-    } else if (ans.classList.contains("shortanswer")) {
-      ans.querySelector(".form-control").value = answers[name];
+  return Object.entries(variants).find((variant) => question.classList.contains(variant[0]))?.[1];
+}
+
+function getLabel(element) {
+  const labelNode = element.querySelector('label').cloneNode(true);
+  labelNode.children[0]?.remove();
+  return labelNode.innerText.replace(/\u00A0|^\s+|\s+$/g, '').trim();
+}
+
+/* ---------------------------------------- src/pasters --------------------------------------------*/
+function pasteChooseAnswer(answer, answerForQuestion) {
+  for (const variant of answer.children) {
+    const label = getLabel(variant);
+    if (!answerForQuestion.includes(label)) {
+      continue;
+    }
+    const checkBox = variant.querySelector(`input:not([type="hidden"])`);
+    checkBox.checked = true;
+  }
+}
+
+function pasteMatchAnswer(answer, answerForQuestion) {
+  const table = answer.children[0];
+
+  for (const row of table.children) {
+    const rowName = row.querySelector('.text p').innerText;
+    const choice = row.querySelector('select');
+    const rightIndex = [...choice.children].findIndex(
+      (el) => el.innerText == answerForQuestion[rowName]
+    );
+    if (rightIndex !== -1) {
+      choice.selectedIndex = rightIndex;
     }
   }
 }
 
-const ta = document.createElement("textarea");
-ta.style.height = "40px";
-const btn = document.createElement("button");
-btn.innerText = "Вставить ответы";
-btn.classList.add("btn", "btn-secondary");
-btn.style.margin = "2px 20px";
-btn.onclick = () => pasteAnsw(ta.value);
-const div = document.createElement("div");
-Object.assign(div.style, {
-  display: "flex",
-  margin: "20px auto",
+function pasteShortAnswer(answer, answerForQuestion) {
+  answer.querySelector('.form-control').value = answerForQuestion;
+}
+
+/* ------------------------------------- src/PasteAnswers.js ----------------------------- */
+function addSearchLink(nameNode) {
+  const url = new URL('https://google.com/search');
+  url.searchParams.set('q', nameNode.innerText);
+
+  const searchButton = document.createElement('button');
+  searchButton.classList.add('google-search-button');
+  const link = document.createElement('a');
+  link.href = url;
+  link.target = '_blank';
+
+  searchButton.append(link);
+  nameNode.append(searchButton);
+}
+
+function setLinks() {
+  getQuestionsNodes('paste').forEach((question) => addSearchLink(question.querySelector('.qtext')));
+}
+
+function pasteAnswersToQuestions(answersJson) {
+  let answers = {};
+  try {
+    answers = JSON.parse(answersJson);
+  } catch (e) {
+    alert('В ходе считывания данных произошла ошибка.\nПодробнее - в консоли.');
+    console.error(e);
+    return;
+  }
+  const questionsNodes = getQuestionsNodes('paste');
+  const handles = {
+    choose: pasteChooseAnswer,
+    match: pasteMatchAnswer,
+    shortanswer: pasteShortAnswer,
+  };
+
+  for (const question of questionsNodes) {
+    const answer = question.querySelector('.answer');
+    const nameNode = question.querySelector('.qtext');
+    const name = nameNode.innerText;
+
+    const answerForQuestion = answers[name];
+    const typeOfQuestion = getQuestionType(question);
+    if (!answerForQuestion || !typeOfQuestion) {
+      continue;
+    }
+
+    handles[typeOfQuestion](answer, answerForQuestion);
+  }
+}
+
+function renderUI() {
+  const pasteButton = document.createElement('button');
+  pasteButton.innerText = 'Вставить ответы';
+  pasteButton.classList.add('btn', 'btn-secondary', 'paste-answers-btn');
+  pasteButton.onclick = () => {
+    navigator.clipboard
+      .readText()
+      .then((value) => {
+        pasteAnswersToQuestions(value);
+      })
+      .catch((err) =>
+        alert(
+          'Во время вставки произошла ошибка, либо буфер обмена пуст.\nПопробуйте кастомный ввод, нажав на шестеренку.'
+        )
+      );
+  };
+
+  const customField = document.createElement('div');
+  customField.classList.add('custom-field-container');
+  customField.innerHTML = `
+    <button class="custom-paste-answers-btn"></button>
+    <textarea class="custom-json-field" placeholder="Введи ответы в JSON формате сюда..."></textarea>
+    <button class="submit-custom-json">Вставить ответы</button>
+  `;
+  customField.querySelector('.custom-paste-answers-btn').onclick = () => {
+    customField.classList.toggle('expanded');
+    pasteButton.classList.toggle('disabled');
+  };
+  customField.querySelector('.submit-custom-json').onclick = () => {
+    pasteAnswersToQuestions(customField.querySelector('textarea').value);
+  };
+
+  const div = document.createElement('div');
+  Object.assign(div.style, {
+    display: 'flex',
+    margin: '20px auto',
+  });
+  div.append(pasteButton, customField);
+  document.querySelector('#region-main').prepend(div);
+}
+
+window.addEventListener('load', () => {
+  renderUI();
+  setLinks();
 });
-div.append(ta, btn);
-document.querySelector("#region-main").prepend(div);
