de579cd328dd9323240dbbea05348afc32c74405
SpringFlash <vadik2001.8928@gmail.com>
2021-12-14 20:31:21 +0300

    build: autopackaging build files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,32 @@
 const { merge } = require('webpack-merge');
+const WebpackCrx = require('webpack-crx');
+const ZipPlugin = require('zip-webpack-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
+
 const config = require('./webpack.config.js');
 const path = require('path');
 
 module.exports = merge(config, {
   mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'build'),
     clean: true,
   },
+  plugins: [
+    new WebpackCrx({
+      key: path.resolve(__dirname, 'key.pem'),
+      src: path.resolve(__dirname, 'dist'),
+      dest: path.resolve(__dirname, 'build'),
+      name: 'sdo-helper',
+    }),
+    new ZipPlugin({
+      path: path.resolve(__dirname, 'build'),
+      filename: 'sdo-helper.zip',
+      pathPrefix: 'sdo-helper',
+    }),
+    new RemovePlugin({
+      before: {
+        include: [path.resolve(__dirname, 'build'), path.resolve(__dirname, 'dist')],
+      },
+    }),
+  ],
 });
